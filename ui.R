7c03eaa5207a5055a4d53e317e10b7d9ab9887d2
Syed Hussain Ather <shussainather@gmail.com>
2023-05-17 20:52:27 -0400

    output

--- ui.R
+++ ui.R
@@ -45,3 +45,12 @@ ui <- navbarPage(
 server <- function(input, output) {
   # assign values to `output` here
 }
+
+output$plot <- renderPlot({
+    
+   ggplot(data=characters, aes_string(x='Character', 
+      y=input$y_var, fill="Class")) +
+      geom_bar(stat="identity", width=0.8) +
+      labs(x="Character", y=input$y_var) + coord_flip()
+    
+})
