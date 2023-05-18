d9dd06e6bd745ee64217792063e2dfa09527a4c0
Syed Hussain Ather <shussainather@gmail.com>
2023-05-17 20:50:00 -0400

    ui html

--- ui.R
+++ ui.R
@@ -4,3 +4,14 @@ library("shiny")
 source("ui.R")
 source("server.R")
 shinyApp(ui = ui, server = server)
+
+intro_panel <- tabPanel(
+  "[Tab Title]",
+  
+  titlePanel("[Page Title]"),
+  
+  img(src = "[img source]"),
+  
+  p("[Summary text for page]"),
+  p(a(href = "[url]", "[Link text]"))
+)
